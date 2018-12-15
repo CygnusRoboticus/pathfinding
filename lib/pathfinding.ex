493729c0adf4b038950f486f2454ab25f84890d0
CygnusRoboticus <joshuadaleharris@gmail.com>
2018-12-15 01:16:35 -0500

    initial commit

---
+++ lib/pathfinding.ex
@@ -0,0 +1,18 @@
+defmodule Pathfinding do
+  @moduledoc """
+  Documentation for Pathfinding.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pathfinding.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pathfinding_test.exs
@@ -0,0 +1,8 @@
+defmodule PathfindingTest do
+  use ExUnit.Case
+  doctest Pathfinding
+
+  test "greets the world" do
+    assert Pathfinding.hello() == :world
+  end
+end
